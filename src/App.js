38153e283ac3a7b004f70abf19347e65370fb1a0
Jared_P <jaredpisell@gmail.com>
2021-06-15 18:07:41 -0700

    Added signin, button, form-input components

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import HomePage from './pages/homepage/home-page.component';
 import ShopPage from './pages/shoppage/shop-page.component';
 import Header from './components/header/header-component.jsx';
+import { signInAndSignUpPage } from './pages/sign-in-and-sign-up.page/sign-in-and-sign-up.component';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
+        <Route path="/signIn" component={signInAndSignUpPage} />
       </Switch>
     </div>
   );
---
+++ src/pages/sign-in-and-sign-up.page/sign-in-and-sign-up.component.jsx
@@ -0,0 +1,11 @@
+import './sign-in-and-sign-up.styles.scss';
+import SignIn from '../../components/sign-in/sign-in.component';
+import React from 'react';
+
+export const signInAndSignUpPage = () => {
+  return (
+    <div className="sign-in-and-sign-up">
+      <SignIn />
+    </div>
+  );
+};
